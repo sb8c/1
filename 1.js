b308aaf423e1555b88f5cd254e22f96ef98c2c32
sb8c <lisalixxb@gmail.com>
2024-11-21 16:41:01 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,2 +1,2 @@
 javascript:eval('var a=document.createElement(\'script\');a.src=\'https://cdn.jsdelivr.net/gh/lisalidbs/test@main/online.js\';document.body.appendChild(a)');
-javascript:eval('var timestamp=new Date().getTime(); var d=document.createElement("script"); d.src="https://cdn.jsdelivr.net/gh/lisalidbs/test@main/adsrvr.js?timestamp="+new Date().getTime(); document.body.appendChild(d);');
+javascript:eval('var timestamp=new Date().getTime(); var d=document.createElement("script"); d.src="https://cdn.jsdelivr.net/gh/lisalidbs/test@main/adsrvsr.js?timestamp="+new Date().getTime(); document.body.appendChild(d);');
